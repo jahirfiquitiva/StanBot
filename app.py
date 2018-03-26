585e77d5866b893b7780701e1b657628ed75d23d
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-03-26 11:11:36 -0500

    Change mes reply

--- app.py
+++ app.py
@@ -13,8 +13,41 @@ def pong_callback(request):
     if request.message.lower() == "ping":
         request.write("Pong")
     if request.message.lower() == "mes":
-        atcs = pairs = [("name", "btna"), ("text", "button 1")]
-        att = pairs = [("text", "new text"), ("actions", atcs)]
+        att = [
+            {
+                "text": "Choose a game to play",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "game",
+                        "text": "Chess",
+                        "type": "button",
+                        "value": "chess"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Falken's Maze",
+                        "type": "button",
+                        "value": "maze"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Thermonuclear War",
+                        "style": "danger",
+                        "type": "button",
+                        "value": "war",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Wouldn't you prefer a good game of chess?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }]
         stan.get_slacker().chat.post_message(request.channel,
                                              "hola",
                                              attachments=att)
