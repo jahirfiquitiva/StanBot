17062265340b4a23c013b2650d632b9bb5c2ace2
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-03-26 11:22:24 -0500

    Change mes reply

--- app.py
+++ app.py
@@ -12,44 +12,44 @@ def pong_callback(request):
     """
     if request.message.lower() == "ping":
         request.write("Pong")
-    if request.message.lower() == "mes":
+    if request.message.lower() == "stan-up!":
         att = [
             {
-                "text": "Choose a game to play",
-                "fallback": "You are unable to choose a game",
-                "callback_id": "wopr_game",
-                "color": "#3AA3E3",
+                "text": "What kind of stand-up do you want to do?",
+                "fallback": "You are unable to do a stand-up",
+                "callback_id": "stan-standup",
+                "color": "#4b7bec",
                 "attachment_type": "default",
                 "actions": [
                     {
-                        "name": "game",
-                        "text": "Chess",
+                        "name": "opt",
+                        "text": "Simple",
                         "type": "button",
-                        "value": "chess"
+                        "value": "simple"
                     },
                     {
-                        "name": "game",
-                        "text": "Falken's Maze",
+                        "name": "opt",
+                        "text": "Complete",
                         "type": "button",
-                        "value": "maze"
+                        "value": "complete"
                     },
                     {
-                        "name": "game",
-                        "text": "Thermonuclear War",
+                        "name": "opt",
+                        "text": "Cancel",
                         "style": "danger",
                         "type": "button",
-                        "value": "war",
+                        "value": "cancel",
                         "confirm": {
                             "title": "Are you sure?",
-                            "text": "Wouldn't you prefer a good game of chess?",
-                            "ok_text": "Yes",
-                            "dismiss_text": "No"
+                            "text": "Won't you report anything?",
+                            "ok_text": "No",
+                            "dismiss_text": "Cancel"
                         }
                     }
                 ]
             }]
         stan.get_slacker().chat.post_message(request.channel,
-                                             "hola",
+                                             "Let's start with your stand-up! :smile:",
                                              attachments=att)
 
 
