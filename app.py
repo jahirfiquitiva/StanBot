4c82a60b3b56fc615b78ce67069aba86100a4cb0
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-03-26 12:04:59 -0500

    Attempt to open dialog

--- app.py
+++ app.py
@@ -60,6 +60,31 @@ def pong_callback(request):
             channel=request.channel,
             text="Hello from Python! :tada:"
         )
+        dail = {
+            "trigger_id": "13345224609.738474920.8088930838d88f008e0",
+            "dialog": {
+                "callback_id": "ryde-46e2b0",
+                "title": "Request a Ride",
+                "submit_label": "Request",
+                "elements": [
+                    {
+                        "type": "text",
+                        "label": "Pickup Location",
+                        "name": "loc_origin"
+                    },
+                    {
+                        "type": "text",
+                        "label": "Dropoff Location",
+                        "name": "loc_destination"
+                    }
+                ]
+            }
+        }
+        sc.api_call(
+            "dialog.open",
+            channel=request.channel,
+            dialog=dail
+        )
 
 
 def main():
