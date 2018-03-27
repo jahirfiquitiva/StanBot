d12d81741d5bea6086002e32bf95a0a1a1004668
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-03-26 20:05:46 -0500

    Fix stop command

--- app.py
+++ app.py
@@ -28,53 +28,8 @@ detail_mess = [
 
 @stan.register("message")
 def callback(request):
-    if reps.active and len(request.message) > 0:
-        valid = reps.reply(request.channel, request.message)
-        if valid:
-            mm = 2 if reps.simple else 3
-            if reps.count() < mm:
-                sc.api_call(
-                    "chat.postMessage",
-                    channel=request.channel,
-                    text=simple_mess[reps.count()] if reps.simple else detail_mess[reps.count()])
-            else:
-                sc.api_call(
-                    "chat.postMessage",
-                    channel=request.channel,
-                    text="Thanks :smile: Your stand-up has finished! :raised_hands:")
-
-                offset = stan.helper_user_id_to_tz_offset(request.user)
-                ft = datetime.datetime.fromtimestamp(request.timestamp + offset).strftime(
-                    '%Y-%m-%d %H:%M:%S')
-
-                sc.api_call(
-                    "chat.postMessage",
-                    channel=REPORTS_CHANNEL,
-                    text="Here's *" + stan.helper_user_id_to_user_real_name(
-                        request.user) + "*'s report from *" + ft + "* (" +
-                         stan.helper_user_id_to_tz_label(request.user) + "):",
-                    attachments=reps.get_as_attachment(),
-                    mrkdwn=True
-                )
-                reps.deactivate()
-        else:
-            extra = [{
-                "text": "Please try using `stan min` or `stan full`. Or try again in a few minutes.",
-                "fallback": "Please try using `stan min` or `stan full`.Or try again in a few minutes.",
-                "mrkdwn": True,
-                "color": "#3AA3E3"
-            }]
-            sc.api_call(
-                "chat.postMessage",
-                channel=request.channel,
-                text="Stan does not seem to be active yet, or someone else is doing a report.",
-                attachments=extra)
-    elif request.message.lower() == "stan min":
-        start_stan(True, request.channel)
-    elif request.message.lower() == "stan full":
-        start_stan(False, request.channel)
-    elif request.message.lower() == "stan stop":
-        if len(reps.channel) > 0:
+    if request.message.lower() == "stan stop":
+        if len(reps.channel) > 0 or reps.active:
             sc.api_call(
                 "chat.postMessage",
                 channel=reps.channel,
@@ -86,26 +41,73 @@ def callback(request):
                 channel=request.channel,
                 text="No stand-up is currently in execution :smile:")
     else:
-        button = [{
-            "text": "Please try using `stan min` or `stan full`",
-            "fallback": "Please try using `stan min` or `stan full`",
-            "mrkdwn": True,
-            "color": "#3AA3E3",
-            "actions": [
-                {
-                    "type": "button",
-                    "text": "Report on GitHub",
-                    "url": BOT_REPO_URL + "/issues/new",
-                    "style": "danger"
-                }
-            ]
-        }]
-        sc.api_call(
-            "chat.postMessage",
-            channel=request.channel,
-            text="Sorry, I'm not that smart yet :disappointed:.",
-            attachments=button,
-            mrkdwn=True)
+        if reps.active and len(request.message) > 0:
+            valid = reps.reply(request.channel, request.message)
+            if valid:
+                mm = 2 if reps.simple else 3
+                if reps.count() < mm:
+                    sc.api_call(
+                        "chat.postMessage",
+                        channel=request.channel,
+                        text=simple_mess[reps.count()] if reps.simple else detail_mess[
+                            reps.count()])
+                else:
+                    sc.api_call(
+                        "chat.postMessage",
+                        channel=request.channel,
+                        text="Thanks :smile: Your stand-up has finished! :raised_hands:")
+
+                    offset = stan.helper_user_id_to_tz_offset(request.user)
+                    ft = datetime.datetime.fromtimestamp(request.timestamp + offset).strftime(
+                        '%Y-%m-%d %H:%M:%S')
+
+                    sc.api_call(
+                        "chat.postMessage",
+                        channel=REPORTS_CHANNEL,
+                        text="Here's *" + stan.helper_user_id_to_user_real_name(
+                            request.user) + "*'s report from *" + ft + "* (" +
+                             stan.helper_user_id_to_tz_label(request.user) + "):",
+                        attachments=reps.get_as_attachment(),
+                        mrkdwn=True
+                    )
+                    reps.deactivate()
+            else:
+                extra = [{
+                    "text": "Please try using `stan min` or `stan full`. Or try again in a few minutes.",
+                    "fallback": "Please try using `stan min` or `stan full`.Or try again in a few minutes.",
+                    "mrkdwn": True,
+                    "color": "#3AA3E3"
+                }]
+                sc.api_call(
+                    "chat.postMessage",
+                    channel=request.channel,
+                    text="Stan does not seem to be active yet, or someone else is doing a report.",
+                    attachments=extra)
+        elif request.message.lower() == "stan min":
+            start_stan(True, request.channel)
+        elif request.message.lower() == "stan full":
+            start_stan(False, request.channel)
+        else:
+            button = [{
+                "text": "Please try using `stan min` or `stan full`",
+                "fallback": "Please try using `stan min` or `stan full`",
+                "mrkdwn": True,
+                "color": "#3AA3E3",
+                "actions": [
+                    {
+                        "type": "button",
+                        "text": "Report on GitHub",
+                        "url": BOT_REPO_URL + "/issues/new",
+                        "style": "danger"
+                    }
+                ]
+            }]
+            sc.api_call(
+                "chat.postMessage",
+                channel=request.channel,
+                text="Sorry, I'm not that smart yet :disappointed:.",
+                attachments=button,
+                mrkdwn=True)
 
 
 def start_stan(simple, chan):
